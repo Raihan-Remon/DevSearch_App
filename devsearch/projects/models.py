8403d0f6c251a26e98168c81e257d4ba7445af53
Raihan-Remon <shamsulraihanremon@gmail.com>
2022-06-22 21:31:25 +0600

    Template Installation

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(
         null=True, blank=True, default="default.jpg")
-    demon_link = models.CharField(max_length=2000, null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
     votes_total = models.IntegerField(default=0, null=True, blank=True)
