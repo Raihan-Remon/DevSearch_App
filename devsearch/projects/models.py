fb3ed71fea683782690221e362c06c35650bd6d1
Raihan-Remon <shamsulraihanremon@gmail.com>
2022-06-25 21:10:53 +0600

    Reviews and Messages Functionality Added

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -24,7 +24,23 @@ class Project(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['-created']
+        ordering = ['-votes_ratio', '-votes_total', 'title']
+
+    @property
+    def reviewers(self):
+        querySet = self.review_set.all().values_list('owner__id', flat=True)
+        return querySet
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value='up').count()
+        totalVotes = reviews.count()
+
+        ratio = (upVotes / totalVotes) * 100
+        self.votes_total = totalVotes
+        self.votes_ratio = ratio
+        self.save()
 
 
 class Review(models.Model):
@@ -32,13 +48,16 @@ class Review(models.Model):
         ('up', 'Up Vote'),
         ('down', 'Down Vote'),
     )
-    # owner =
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     value = models.CharField(max_length=200, choices=VOTE_TYPES)
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4,
                           primary_key=True, editable=False, unique=True)
+
+    class Meta:
+        unique_together = [['owner', 'project']]
 
     def __str__(self):
         return self.value
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 from .models import Project, Tag
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.db.models import Q
 from .utils import searchProjects, paginateProjects
 # Create your views here.
@@ -19,7 +19,23 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(pk=pk)
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        # Update project vote count
+
+        projectObj.getVoteCount
+        messages.success(
+            request, "Your review has been successfully submitted!")
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
 
 
 @login_required(login_url='login')
